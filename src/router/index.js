3440bc7b8c3a556c8f9bbeeb3e3b4372a45e2179
Edu <30601020+eduardocoello16@users.noreply.github.com>
2022-05-29 18:08:55 +0100

    historyhashmode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import PageNotFound from '../views/PageNotFound.vue'
 
 const routes = [
@@ -74,7 +74,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
